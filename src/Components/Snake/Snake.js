bbf6b40c9a0c0265bdca8f4ebcd4f915b61677a9
SadatArefin <sadatarefin@iut-dhaka.edu>
2021-10-21 16:56:42 +0600

    Snake Game

--- src/Components/Snake/Snake.js
+++ src/Components/Snake/Snake.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Snake = () => {
-    return (
-        <div>
-            <iframe src="https://game-it-out.netlify.app/games/snake/index.html" height="600px" className="mx-auto mt-5 w-3/4" title="snake" frameborder="0"></iframe>
-        </div>
-    );
+  return (
+    <div>
+      <iframe
+        src="https://game-it-out.netlify.app/games/snake/index.html"
+        height="600px"
+        className="mx-auto mt-5 w-3/4"
+        title="snake"
+        frameborder="0"
+      ></iframe>
+    </div>
+  );
 };
 
 export default Snake;
