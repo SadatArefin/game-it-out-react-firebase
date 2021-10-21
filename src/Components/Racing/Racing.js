93145a93080841ad72bdd0105f2dd610d91dcf91
SadatArefin <sadatarefin@iut-dhaka.edu>
2021-10-21 16:57:37 +0600

    Futuristic Racing

---
+++ src/Components/Racing/Racing.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Racing = () => {
+  return (
+    <div>
+      <iframe
+        src="https://game-it-out.netlify.app/games/racing/index.html"
+        height="600px"
+        className="mx-auto mt-5 w-3/4"
+        title="snake"
+        frameborder="0"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Racing;
