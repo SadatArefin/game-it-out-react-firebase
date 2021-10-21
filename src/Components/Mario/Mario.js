278ab06e4ec4589369cbee98aed397c82ae1e771
SadatArefin <sadatarefin@iut-dhaka.edu>
2021-10-21 17:19:56 +0600

    Mario

---
+++ src/Components/Mario/Mario.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Mario = () => {
+  return (
+    <div>
+      <iframe
+        src="https://srijanreddy.github.io/Games/mario/index.html"
+        height="600px"
+        className="mx-auto mt-5 w-3/4"
+        title="snake"
+        frameborder="0"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Mario;
