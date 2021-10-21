f79f6bc599a1bf8cacb79566ca8f81039604cb58
SadatArefin <sadatarefin@iut-dhaka.edu>
2021-10-21 17:19:45 +0600

    Pacman

---
+++ src/Components/Pacman/Pacman.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Pacman = () => {
+  return (
+    <div>
+      <iframe
+        src="https://srijanreddy.github.io/Games/gamescoll%20html5%20js/pacman/index.html"
+        height="600px"
+        className="mx-auto mt-5 w-3/4"
+        title="snake"
+        frameborder="0"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Pacman;
