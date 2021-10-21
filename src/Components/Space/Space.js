71984039cda6a6ce6312d5003a22fcc51ceafe7c
SadatArefin <sadatarefin@iut-dhaka.edu>
2021-10-21 16:57:03 +0600

    Space Invader

---
+++ src/Components/Space/Space.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Space = () => {
+  return (
+    <div>
+      <iframe
+        src="https://game-it-out.netlify.app/games/space/index.html"
+        height="600px"
+        className="mx-auto mt-5 w-3/4"
+        title="snake"
+        frameborder="0"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Space;
