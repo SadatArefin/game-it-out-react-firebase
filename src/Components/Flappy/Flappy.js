8a1c09b0daf34695d39dc16bfdab6450ebc2c888
SadatArefin <sadatarefin@iut-dhaka.edu>
2021-10-21 16:57:48 +0600

    Flappy Bird

---
+++ src/Components/Flappy/Flappy.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Flappy = () => {
+    return (
+        <div>
+             <iframe
+        src="https://game-it-out.netlify.app/games/flappy/index.html"
+        height="600px"
+        className="mx-auto mt-5 w-3/4"
+        title="snake"
+        frameborder="0"
+      ></iframe>
+        </div>
+    );
+};
+
+export default Flappy;
